6b585f79818ad75f0afdfde522518e4986628e81
Eric Scott <scottericr@gmail.com>
2024-10-24 14:11:37 -0700

    test renv.sh

---
+++ renv.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=renv_restore
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem-per-cpu=5gb          
+#SBATCH --time=4:00:00
+#SBATCH --partition=standard
+#SBATCH --account=kristinariemer
+#SBATCH -o logs/%x_%j.out
+
+# Load necessary modules
+module load R/4.3
+
+# Sync with renv.lock using `pak` for installation
+export RENV_CONFIG_PAK_ENABLED=TRUE
+R -e 'renv::restore()'
